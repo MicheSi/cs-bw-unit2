5f198149395f11c2b3e8e9e39577ce51965a0a7d
mlazzell17@gmail.com <MicheSi>
2020-06-24 15:02:19 -0700

    completed merge_two_sorted_lists

--- merge_two_sorted.py
+++ merge_two_sorted.py
@@ -6,3 +6,27 @@ Example:
 Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
 '''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # if both ll exist
+        if l1 and l2:
+            # check values of 1st node in each list
+            # put the smaller value on the left
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            # if there is a next node, use recursion to check through the other nodes
+            if l1.next:
+                return ListNode(l1.val, self.mergeTwoLists(l1.next, l2))
+                # no more nodes to check, so return the linked list
+            else:
+                return ListNode(l1.val, l2)
+        # if linked list doesn't exist, return the other one
+        elif not l1:
+            return l2
+        elif not l2:
+            return l1
