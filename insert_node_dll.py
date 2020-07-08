b72ef49d5d6403b9dab77875320f86237b649328
mlazzell17@gmail.com <MicheSi>
2020-07-08 11:57:19 -0700

    insert node into sorted dll

---
+++ insert_node_dll.py
@@ -0,0 +1,20 @@
+def sortedInsert(head, data):
+    cur=head
+    node=DoublyLinkedListNode(data)
+    if cur.data>data or cur.data==data:
+        node.next=cur
+        cur.prev=node
+        head=node
+        return head
+    while cur.next:
+        if (cur.data<data and cur.next.data>data) or cur.data==data:
+            node.next=cur.next
+            cur.next.prev=node
+            node.prev=cur
+            cur.next=node
+            return head
+        cur=cur.next
+    if cur.data<data or cur.data==data:
+        node.prev=cur
+        cur.next=node
+        return head
